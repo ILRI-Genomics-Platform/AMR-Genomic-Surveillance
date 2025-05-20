f086c458a02d037f8150de55f208892b7c7d4821
ajodeh-juma <ajodeh.juma@gmail.com>
2025-05-20 11:02:09 +0300

    add sbatch script for AMR visualization

---
+++ scripts/visualizeAMR.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH -w compute06
+#SBATCH --job-name=plot-phylo-amr
+#SBATCH --output=stdout_%j.out
+#SBATCH --error=stderr_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+#SBATCH --partition=batch
+
+
+# load the required module(s)
+module load R/4.2
+
+
+# I/O
+WORKDIR="/var/scratch/$USER/ACDC_AMR2025"
+OUTDIR="${WORKDIR}/results/ont/klebsiella"
+
+Rscript ./scripts/visualizeAMR.R \
+    --tree ${OUTDIR}/iqtree/core-snp.treefile \
+    --mlst ${OUTDIR}/mlst/*.tsv \
+    --pointfinder ${OUTDIR}/resfinder/*/PointFinder_results.txt \
+    --resfinder ${OUTDIR}/resfinder/*/ResFinder_results_tab.txt \
+    --prefix phylogeny-amr \
+    --outdir ${OUTDIR}/plots
